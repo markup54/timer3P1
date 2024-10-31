cd7bf36486b7f024e87c4d1ee7eb89868e9b2ded
markup54 <marzenakupka@tlen.pl>
2024-10-31 12:12:17 +0100

    poczatek lekcji

---
+++ app/src/main/java/pl/zabrze/zs10/timer3p1/SzachyActivity.java
@@ -0,0 +1,14 @@
+package pl.zabrze.zs10.timer3p1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SzachyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_szachy);
+    }
+}
